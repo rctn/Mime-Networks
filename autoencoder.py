0a2ea6e218da11e5ac189d01a2a0c202e17bf51e
Jascha Sohl-Dickstein <jascha.sohldickstein@gmail.com>
2014-02-22 19:53:50 -0800

    added the HiddenLayer class

--- autoencoder.py
+++ autoencoder.py
@@ -79,3 +79,71 @@ class autoencoder:
 
 		self.theta_init = [param.get_value() for param in params]
 		self.theta = [param.get_value() for param in params]
+
+
+class HiddenLayer(object):
+	def __init__(self, rng, input, n_in, n_out, W=None, b_mean=0.,
+				 activation=T.tanh, scl=1.):
+		"""
+		Typical hidden layer of a MLP: units are fully-connected and have
+		sigmoidal activation function. Weight matrix W is of shape (n_in,n_out)
+		and the bias vector b is of shape (n_out,).
+
+		NOTE : The nonlinearity used here is tanh
+
+		Hidden unit activation is given by: tanh(dot(input,W) + b)
+
+		:type rng: numpy.random.RandomState
+		:param rng: a random number generator used to initialize weights
+
+		:type input: theano.tensor.dmatrix
+		:param input: a symbolic tensor of shape (n_examples, n_in)
+
+		:type n_in: int
+		:param n_in: dimensionality of input
+
+		:type n_out: int
+		:param n_out: number of hidden units
+
+		:type activation: theano.Op or function
+		:param activation: Non linearity to be applied in the hidden
+						   layer
+		"""
+		self.input = input
+
+		self.n_in = n_in
+		self.n_out = n_out
+
+		# `W` is initialized with `W_values` which is uniformely sampled
+		# from sqrt(-6./(n_in+n_hidden)) and sqrt(6./(n_in+n_hidden))
+		# for tanh activation function
+		# the output of uniform if converted using asarray to dtype
+		# theano.config.floatX so that the code is runable on GPU
+		# Note : optimal initialization of weights is dependent on the
+		#        activation function used (among other things).
+		#        For example, results presented in [Xavier10] suggest that you
+		#        should use 4 times larger initial weights for sigmoid
+		#        compared to tanh
+		#        We have no info for other function, so we use the same as
+		#        tanh.
+		if W is None:
+			W_values = np.asarray(rng.uniform(
+					low=-np.sqrt(1. / (n_in)),
+					high=np.sqrt(1. / (n_in)),
+					size=(n_in, n_out)), dtype=theano.config.floatX) * scl
+			if activation == theano.tensor.nnet.sigmoid:
+				W_values *= 4
+
+			W = theano.shared(value=W_values, name='W', borrow=True)
+
+		b_values = b_mean*np.ones((n_out,), dtype=theano.config.floatX)
+		b = theano.shared(value=b_values, name='b', borrow=True)
+
+		self.W = W
+		self.b = b
+
+		lin_output = T.dot(input, self.W) + self.b
+		self.output = (lin_output if activation is None
+					   else activation(lin_output))
+		# parameters of the model
+		self.params = [self.W, self.b]
