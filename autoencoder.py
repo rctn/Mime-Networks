87a5328989bb7a277495917a2cd62512e93859d9
briancheung <bcheung5@gmail.com>
2014-03-09 22:38:05 -0700

    Autoencoder __init__ made runnable

--- autoencoder.py
+++ autoencoder.py
@@ -3,7 +3,8 @@ import theano.tensor as T
 import numpy as np
 
 class autoencoder:
-    def __init__(self, data, num_subfunctions_ratio=0.05, ntrain=1e6, patch_width=8, sigma=0.1):
+    def __init__(self, data, num_subfunctions_ratio=0.05, ntrain=1e6, 
+        patch_width=8, sigma=0.1, overcomplete=2, batch_size=200, rng=np.random.RandomState(23455)):
         self.name = 'Denoising Autoencoder with vanHat'
 
         # standard deviation of noise to use for denoising autoencoder
@@ -13,10 +14,15 @@ class autoencoder:
         self.data = data
         self.patch_size = data.patch_size
 
+        # Factor of overcompleteness for hidden units
+        self.overcomplete = overcomplete
+        self.rng = rng
+
         # create objective function, and initialize parameters
         self.build_model()
 
         # break the data up into minibatches
+
         self.N = int(np.ceil(np.sqrt(batch_size)*num_subfunctions_ratio))
         self.subfunction_references = []
         samples_per_subfunction = int(np.floor(batch_size/self.N))
@@ -25,24 +31,21 @@ class autoencoder:
             end_idx = (mb+1)*samples_per_subfunction
             self.subfunction_references.append((start_idx, end_idx,))
         #self.full_objective_references = self.subfunction_references
-        self.full_objective_references = random.sample(self.subfunction_references, int(num_subfunctions/100.))
+        #self.full_objective_references = self.rng.sample(self.subfunction_references, int(num_subfunctions/100.))
 
 
     def f_df(self, theta, irange, holdout=False):
         X = self.data.get_data_range(irange[0], irange[1], holdout=holdout)
 
         # generate noise to add to X in a deterministic fashion based on irange
-        rand_state = np.random.get_state()
-        np.random.seed(irange)
-        N = np.random.randn(X.shape[0], X.shape[1])*self.sigma
-        np.random.set_state(rand_state)
+        N = self.rng.randn(X.shape[0], X.shape[1])*self.sigma
 
         results = self.f_df_Theano(*(theta + [X, N]))
         return results[0], results[1:]
 
     def build_model(self):
         # repeatable experiments
-        rng = np.random.RandomState(1234)
+        rng = self.rng
 
         X = T.matrix('x')
         N = T.matrix('n')
@@ -63,7 +66,7 @@ class autoencoder:
         params += L3.params
 
         # reconstruction error
-        err = ((L3 - X)**2).mean()
+        err = ((L3.output - X)**2).mean()
 
         # compute the gradient of cost with respect to theta (sorted in params)
         # the resulting gradients will be stored in a list gparams
